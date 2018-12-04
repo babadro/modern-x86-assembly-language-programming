10bf4d6c2400733a9a9d3d30e8e0fbe8b314aadc
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-12-04 08:21:24 +0300

    CalsSum_ complete

--- 978-1-4842-0065-0_SourceCode/Chapter02/CalcSum/CalcSum/CalcSum.cpp
+++ 978-1-4842-0065-0_SourceCode/Chapter02/CalcSum/CalcSum/CalcSum.cpp
@@ -4,12 +4,11 @@ extern "C" int CalcSum_(int a, int b, int c);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    int a = 17, b = 11, c = 14;
-    int sum = CalcSum_(a, b, c);
+	int a = 17, b = 11, c = 14;
+	int sum = CalcSum_(a, b, c);
 
-    printf("  a:   %d\n", a);
-    printf("  b:   %d\n", b);
-    printf("  c:   %d\n", c);
-    printf("  sum: %d\n", sum);
-    return 0;
+	printf(" a:    %d\n", a);
+	printf(" b:    %d\n", b);
+	printf(" c:    %d\n", c);
+	printf(" sum:    %d\n", sum);
 }
