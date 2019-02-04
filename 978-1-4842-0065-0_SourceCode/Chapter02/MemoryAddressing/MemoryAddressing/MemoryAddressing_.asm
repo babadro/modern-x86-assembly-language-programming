c6480eb005d4256cea103fbf8f7960484aca06a2
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2019-02-04 08:37:28 +0300

    MemoryAddressing works

--- 978-1-4842-0065-0_SourceCode/Chapter02/MemoryAddressing/MemoryAddressing/MemoryAddressing_.asm
+++ 978-1-4842-0065-0_SourceCode/Chapter02/MemoryAddressing/MemoryAddressing/MemoryAddressing_.asm
@@ -1,4 +1,4 @@
-	.model flat,call
+			.model flat,c
 
 ; Simple lookup table (.const section data is read only)
 	
@@ -29,11 +29,11 @@ MemoryAddressing_ proc
 
 ; Make sure 'i' is valid
 	xor eax,eax
-	mov ecx,[ebp+8]
+	mov ecx,[ebp+8]				;ecx = i
 	cmp ecx,0
-	jl InvalidIndex
+	jl InvalidIndex				;jump if i < 0
 	cmp ecx,[NumFibVals_]
-	jge InvalidIndex
+	jge InvalidIndex			;jump if i >= NumFibVals_
 
 ; Example #1 - base register
 	mov ebx,offset FibVals		;ebx = FibVals
@@ -44,9 +44,35 @@ MemoryAddressing_ proc
 	mov edi,[ebp+12]
 	mov [edi],eax				;Save to 'v1'
 
+; Example #2 - base register + displacement
+; esi is used as the base
+	mov esi,[ebp+8]				;esi = i
+	shl esi,2					;esi = i * 4
+	mov eax,[esi+FibVals]		;Load table value
+	mov edi,[ebp+20]
+	mov [edi],eax				;Save to 'v3'
+
+; Example #3 - base register + index register * scale factor
+	mov ebx,offset FibVals		;ebx = FibVals
+	mov esi,[ebp+8]				;esi = i
+	shl esi,2					;esi = i * 4
+	mov eax,[ebx+esi]			;Load table value
+	mov edi,[ebp+20]
+	mov [edi],eax				;Save to 'v3'
+
+; Example #4 - base register + index register * scale factor
+	mov ebx,offset FibVals		;ebx = FibVals
+	mov esi,[ebp+8]				;esi = i
+	mov eax,[ebx+esi*4]			;Load table value
+	mov edi,[ebp+24]
+	mov [edi],eax				;Save to 'v4'
+	mov eax,1					;Set return code
+
 InvalidIndex:
 	pop edi
 	pop esi
 	pop ebx
 	pop ebp
 	ret
+MemoryAddressing_ endp
+	end
